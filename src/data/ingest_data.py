f134542291f748d0377e579bf1c875c231e15384
David Echeverri Montoya <95106005+decheverrim@users.noreply.github.com>
2022-07-02 17:52:38 -0500

    ok

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -13,10 +13,25 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
+    
+
+    import requests
+
+    for year in range(1995, 2022):
+        if year in range(2016, 2018):
+            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(year)
+            file = requests.get(url, allow_redirects=True)
+            open('data_lake/landing/{}.xls'.format(year), 'wb').write(file.content)
+        else:
+            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(year)
+            file = requests.get(url, allow_redirects=True)
+            open('data_lake/landing/{}.xlsx'.format(year), 'wb').write(file.content)
+    return
     raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
+    ingest_data()
 
     doctest.testmod()
