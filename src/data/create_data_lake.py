29ff7fc3dc1dc0750957e06e340cb5fa20afb7a8
David Echeverri Montoya <95106005+decheverrim@users.noreply.github.com>
2022-07-02 17:26:12 -0500

    ok

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -1,8 +1,6 @@
-from multiprocessing import parent_process
-
-
+import os
 def create_data_lake():
-    import os
+    
     """Cree el data lake con sus capas.
 
     Esta función debe crear la carpeta `data_lake` en la raiz del proyecto. El data lake contiene
@@ -24,28 +22,40 @@ def create_data_lake():
     ```
  
     """
-    #Create data lake
-    os.mkdir("data_lake")
+    #Create the parent directory in the root of the proyect
     parent_directory = "data_lake"
-
-    #Delete if data_lake already exist
     cwd = os.getcwd()
-    path_data_lake = os.path.join(cwd, parent_directory)
-    if os.path.isdir(path_data_lake):
-        os.system("rm -rf "+ parent_directory)
+    path_parent_directory = os.path.join(cwd,parent_directory)
     
-    #Create directory
+    #Remove if parent directory already exist
+    if os.path.isdir(path_parent_directory):
+        os.system("rm -rf " + path_parent_directory)
+        os.mkdir(path_parent_directory)
+    else:
+        os.mkdir(path_parent_directory)
+    
+    #Create the directory's names
     directorys = ["landing","raw","cleansed","business"]
     directorys_business = ["business/reports","business/reports/figures","business/features","business/forecasts"]
-    for directory in directorys:
-        os.mkdir(os.path.join("data_lake",directory))
-    for directory_business in directorys_business:
-        os.mkdir(os.path.join("data_lake",directory_business))
-    
-    #raise NotImplementedError("Implementar esta función")
+
+    #Create the directorys
+    for directory_name in directorys:
+        os.mkdir(os.path.join(path_parent_directory, directory_name))
+
+    for directory_name_business in directorys_business:
+        os.mkdir(os.path.join(path_parent_directory, directory_name_business))
 
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+
     create_data_lake()
+    doctest.testmod()    
+
+
+
+    
+    #raise NotImplementedError("Implementar esta función")
+
+
+
