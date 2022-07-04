d59e2497d3fbeba5ac8bd54dd66edfd5d56808cc
David Echeverri Montoya <95106005+decheverrim@users.noreply.github.com>
2022-07-04 11:32:41 -0500

    Correct

--- grader.py
+++ grader.py
@@ -140,7 +140,7 @@ def test_11():
 
 def test_12():
     """Pronosticos"""
-    os.system("make train_model")
+    os.system("make make_forecasts")
     assert os.path.isfile("data_lake/business/forecasts/precios-diarios.csv") is True
 
 
