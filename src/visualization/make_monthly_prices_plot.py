ab2f72f31a1c947ffc8fdaac5b08c33043d11bc2
David Echeverri Montoya <95106005+decheverrim@users.noreply.github.com>
2022-07-04 05:21:35 -0500

    ok

---
+++ src/visualization/make_monthly_prices_plot.py
@@ -0,0 +1,18 @@
+
+def make_monthly_prices_plot():
+    """Crea un grafico de lines que representa los precios promedios mensuales.
+    Usando el archivo data_lake/business/monthly_prices.csv, crea un grafico de
+    lines que representa los precios promedios mensuales.
+    El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/monthly_prices.png.
+    """
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    Monthly_prices = pd.read_csv("./data_lake/business/precios-mensuales.csv")
+    plt.plot(Monthly_prices["fecha"], Monthly_prices["precio"])
+    plt.savefig('./data_lake/business/reports/figures/monthly_prices.png')
+
+
+if __name__ == "__main__":
+    import doctest
+    make_monthly_prices_plot()
+    doctest.testmod()
